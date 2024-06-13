85ec0906f99f24c4d85a439af557b674294edd2c
MeiKaiAoXue <22301016@bjtu.edu.cn>
2024-06-13 22:09:37 +0800

    add DataTimeUtil

--- api/src/main/java/com/movie/api/utils/DataTimeUtil.java
+++ api/src/main/java/com/movie/api/utils/DataTimeUtil.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 
 public final class DataTimeUtil {
 
+
     public static String getNowTimeString() {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return df.format(System.currentTimeMillis());
